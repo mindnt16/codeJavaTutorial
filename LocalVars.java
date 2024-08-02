a00f58f15fc8d0c3397ba43bd207071ef7e13c37
mindnt16 <152350734+mindnt16@users.noreply.github.com>
2024-08-02 14:13:10 +0700

    Create LocalVars.java

---
+++ LocalVars.java
@@ -0,0 +1,16 @@
+public class LocalVars {
+    public static void main(String[] args) {
+        phuker();
+        rayoug();
+        phuker();
+    }
+    public static void phuker(){
+        int birds = 5000;
+        System.out.println("In phuket there are " +birds+ " birds.");
+    }
+    public static void rayoug(){
+        int birds = 3500;
+        System.out.println("In rayoug there are " +birds+ " birds.");
+    }
+}
+
