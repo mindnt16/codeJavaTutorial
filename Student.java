4e84eac3e4ef3d3b84582249bc43de8bee6f5ceb
mindnt16 <152350734+mindnt16@users.noreply.github.com>
2024-08-08 12:54:49 +0700

    Create Student.java

---
+++ Student.java
@@ -0,0 +1,24 @@
+public class Student {
+   private String id;
+   private String name;
+   private double gpa;
+    public void setDetails(String ID, String n, double GPA){
+id = ID;
+name = n;
+gpa = GPA;
+    }
+    public void showDetails(){
+        System.out.println("ID : " +id);
+        System.out.println("Name : " +name);
+        System.out.println("GPA : " +gpa);
+    }
+    public static void main(String[] args) {
+        Student sc1 = new Student();
+        sc1.setDetails("23456", "Nawarat", "4.0");
+        sc1.showDetails();
+        Student sc2 = new Student();
+        sc2.setDetails("23456", "Nawarat", "4.0");
+        sc2.showDetails();
+    }
+    }
+
